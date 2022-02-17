8421d74e66f6f56a3d9b2bc69108c55ede4dd265
Mathieson Sterling <mathieson.sterling@gmail.com>
2022-02-17 16:04:13 -0500

    Move deser to service

--- lib/siy/siy/repositories/gcp_firestore/firestore_connection_repository.py
+++ lib/siy/siy/repositories/gcp_firestore/firestore_connection_repository.py
@@ -2,32 +2,21 @@ from typing import Iterable, Optional, Dict, Any
 
 from google.cloud import firestore
 
-from siy.entities import Connection, BaseDestination, BaseSource
+from siy.entities import Connection
 from siy.repositories.connection_repository import BaseConnectionRepository
+from siy.services import DeserializationService
 
 
 class FirestoreConnectionRepository(BaseConnectionRepository):
-    def __init__(self, project_id: str):
+    def __init__(self, project_id: str, deserialization_service: DeserializationService = None):
         self.db = firestore.Client(project=project_id)
 
-    def _hydrate_source(self, dict_: Dict[str, Any]) -> BaseSource:
-        pass
+        if not deserialization_service:
+            deserialization_service = DeserializationService()
+        self.deserialization_service = deserialization_service
 
-    def _hydrate_destination(self, dict_: Dict[str, Any]) -> BaseDestination:
-        pass
-
-    def _hydrate_connection(self, dict_: Dict[str, Any]) -> Connection:
-        # hydrate the sources
-        sources = [self._hydrate_source(d) for d in dict_["sources"]]
-
-        # hydrate the destinations
-        destinations = [self._hydrate_destination(d) for d in dict_["destinations"]]
-
-        return Connection(
-            name=dict_["name"],
-            sources=sources,
-            destinations=destinations
-        )
+    def _hydrate_connection(self, dict_) -> Connection:
+        return self.deserialization_service.deserialize_connection(dict_)
 
     @property
     def _collection(self):
@@ -35,7 +24,7 @@ class FirestoreConnectionRepository(BaseConnectionRepository):
 
     def get_all(self) -> Iterable[Connection]:
         for d in self._collection.stream():
-            yield self._hydrate_destination(dict_=d.to_dict())
+            yield self._hydrate_connection(dict_=d.to_dict())
 
     def get(self, connection_name: str) -> Optional[Connection]:
         doc_ref = self._collection.document(connection_name)
