b16bb0b390d3e45cb1475a136f23c4ddd79c2b7c
Mathieson Sterling <mathieson.sterling@gmail.com>
2022-02-21 12:36:06 -0500

    Cleanup for versions

--- lib/siy/siy/value_items/__init__.py
+++ lib/siy/siy/value_items/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa
-from siy.value_items.datatable import BaseDataTable, BigQueryDataTable
 from siy.value_items.docker_task import DockerTask
 from siy.value_items.published_state import PublishedState
 from siy.value_items.url import URL
